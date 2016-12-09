3b3f4d9fb011d4d7001fae3824b1e1dcf0c4b992
Miguel Reboiro-Jato <mrjato@uvigo.es>
2016-12-09 12:09:23 +0100

    Adds a DAO for Pfam entity
    The PfamDAO allows Pfam retrieval by accession number.

---
+++ src/main/java/es/uvigo/ei/sing/pandrugsdb/persistence/dao/DefaultPfamDAO.java
@@ -0,0 +1,57 @@
+/*
+ * #%L
+ * PanDrugsDB Backend
+ * %%
+ * Copyright (C) 2015 - 2016 Fátima Al-Shahrour, Elena Piñeiro, Daniel Glez-Peña and Miguel Reboiro-Jato
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package es.uvigo.ei.sing.pandrugsdb.persistence.dao;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.uvigo.ei.sing.pandrugsdb.persistence.entity.Pfam;
+
+@Repository
+@Transactional
+public class DefaultPfamDAO implements PfamDAO {
+	@PersistenceContext
+	private EntityManager em;
+	
+	private DAOHelper<String, Pfam> dh;
+	
+	DefaultPfamDAO() {}
+	
+	public DefaultPfamDAO(EntityManager em) {
+		this.em = em;
+		createDAOHelper();
+	}
+	
+	@PostConstruct
+	private void createDAOHelper() {
+		this.dh = DAOHelper.of(String.class, Pfam.class, em);
+	}
+	
+	@Override
+	public Pfam get(String accession) {
+		return dh.get(accession.toUpperCase());
+	}
+}
